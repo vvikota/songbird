3c69c4a1614ffd93c6c225fcf21f9c28854dce8b
vvikot <rymsha.viktor@gmail.com>
2020-07-31 18:01:56 +0300

    feat: add redux

---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  currenQuestion: `question`
+}
+
+const ActionCreator = {
+
+}
+
+const reducer = (state = initialState , action) => {
+  switch (action.type) {
+    case 
+  }
+
+  return state;
+};
+
+export {reducer, ActionCreator};
