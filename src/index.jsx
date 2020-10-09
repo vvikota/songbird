5a3f16da3dfb399ddfdd17d8cee4cb2a965b4f3e
Vi <rymsha.viktor@gmail.com>
2020-10-09 12:32:26 +0300

    feat: integrate persist-redux in App

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {createStore} from "redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react'
 import {Provider} from "react-redux";
 
 import App from './App.tsx';
@@ -9,16 +12,29 @@ import reducer from "./reducer/index.js";
 import {ActionCreator as MainActionCreator} from "./reducer/main/main";
 
 const init = () => {
+  
+  const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['questions']
+  }
+
+  const persistedReducer = persistReducer(persistConfig, reducer)
+
   const store = createStore(
-    reducer,
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  const persistor = persistStore(store)
 
   store.dispatch(MainActionCreator.loadQuestions());
 
   ReactDOM.render(
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>,
     document.getElementById('root')
   );
